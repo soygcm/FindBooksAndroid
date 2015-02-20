aba0db4096a79fb4785c5ba619d0165f0ba06958
Daniel Castañaza <danipin.2023@gmail.com>
2015-02-19 23:12:28 -0600

    Perfil

--- src/me/mobileease/findbooks/PerfilActivity.java
+++ src/me/mobileease/findbooks/PerfilActivity.java
@@ -2,6 +2,7 @@ package me.mobileease.findbooks;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 public class PerfilActivity extends ActionBarActivity {
 	
@@ -9,6 +10,14 @@ public class PerfilActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_perfil);
 		
+		 Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+	        if (toolbar != null) {  		
+	        		setSupportActionBar(toolbar);
+	        }
+	        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
 	}
 
 }
+
+
